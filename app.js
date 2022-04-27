b75ad5c1990ecaffc4c75f634aaeae13c975ac59
Darrel Baerwaldt <darrel.baerwaldt@hotmail.com>
2022-04-27 23:33:39 +0900

    commit befor pulling latest

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-require("dotenv").config();
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const hbs = require("hbs");
+require('dotenv').config();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const hbs = require('hbs');
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-const productRouter = require("./routes/products");
-const addProductRouter = require("./routes/addProduct");
-const volunteerRouter = require("./routes/volunteer")
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const productRouter = require('./routes/products');
+const addProductRouter = require('./routes/addProduct');
+const volunteerRouter = require('./routes/volunteer');
 
 const app = express();
 
@@ -23,25 +23,25 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then((res) => console.log("db connected"))
+  .then((res) => console.log('db connected'))
   .catch((err) => console.log(err));
 
 // view engine setup
-hbs.registerPartials(__dirname + "/views/partials");
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/products", productRouter)
-app.use("/products/add", addProductRouter);
-app.use("/volunteer", volunteerRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/products', productRouter);
+app.use('/products/add', addProductRouter);
+app.use('/volunteer', volunteerRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -52,11 +52,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
