033963b64d1ab6697513c8f824c044a9c423e708
thomasmama14 <92804690+thomasmama14@users.noreply.github.com>
2022-04-30 21:22:13 -0400

    cart Schema and Route successfully created

--- routes/products.js
+++ routes/products.js
@@ -5,7 +5,7 @@ let router = express.Router();
 router.get("/", async (req, res) => {
    let products;
    products = await Product.find()
-   console.log(products)
+   //console.log(products)
    res.render("products", {title: "Products", products})
 })
 
