cbc4c0843af63ef3e396f6a7e38f434bbf5cb80a
Darrel Baerwaldt <darrel.baerwaldt@hotmail.com>
2022-05-03 12:17:47 +0900

    pulling latest

--- routes/events.js
+++ routes/events.js
@@ -2,50 +2,51 @@ const express = require('express');
 const router = express.Router();
 const Event = require('../models/Event');
 
-router.get('/', async function(req, res) {
-    console.log('events is firing.')
+router.get('/', async function (req, res) {
+  console.log('events is firing.');
+  let loggedIn = false;
+  if (req.cookies.loggedIn) {
+    loggedIn = true;
+  }
 
-    const events = await Event.find({})
-    .lean()
-    .sort('date')
+  const events = await Event.find({}).lean().sort('date');
 
-    res.render('events', {events}); 
-})
+  res.render('events', { events, loggedIn });
+});
 
 router.post('/', async (req, res) => {
+  const { event, date, description, imageUrl } = req.body;
+  console.log(req.body);
+  console.log(event, date, description, imageUrl);
 
-    const { event, date, description, imageUrl } = req.body;
-    console.log(req.body);
-    console.log(event, date, description, imageUrl);
+  const newEvent = new Event({
+    event: event,
+    date: date,
+    description: description,
+    imageUrl: imageUrl,
+  });
 
-    const newEvent = new Event({
-        event: event,
-        date: date,
-        description: description,
-        imageUrl: imageUrl
-    });
-
-    await newEvent.save((err) => {
-        if(err) {
-            const error = newEvent.validateSync().errors;
-            if(error.event) {
-                res.render('events', {message: error.event.message})
-            }
-            if(error.date) {
-                res.render('events', {message: error.date.message})
-            }
-            if(error.description) {
-                res.render('events', {message: error.description.message})
-            }
-        } else {
-            console.log("New event saved!")
-            res.redirect('events'); 
-        }
-    })
-})
+  await newEvent.save((err) => {
+    if (err) {
+      const error = newEvent.validateSync().errors;
+      if (error.event) {
+        res.render('events', { message: error.event.message });
+      }
+      if (error.date) {
+        res.render('events', { message: error.date.message });
+      }
+      if (error.description) {
+        res.render('events', { message: error.description.message });
+      }
+    } else {
+      console.log('New event saved!');
+      res.redirect('events');
+    }
+  });
+});
 
 router.get('/signUp', (req, res) => {
-    console.log('firing!')
-})    
+  console.log('firing!');
+});
 
-module.exports = router; 
+module.exports = router;
