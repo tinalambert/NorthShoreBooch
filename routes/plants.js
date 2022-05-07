d2ad5fd0bf6b93dd8018be140f43b5b85882bed9
thomasmama14 <92804690+thomasmama14@users.noreply.github.com>
2022-05-07 18:39:40 -0400

    Added plants data, schema, hbs, js, and garden hbs

---
+++ routes/plants.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const Plant = require("../models/Plant");
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET;
+
+router.get("/", async (req, res) => {
+  //const { ScientificName, CommonName, State, Duration, GrowthRate, AdaptedCoarseSoils, AdaptedMediumSoils, AdaptedFineSoils, ShadeTolerance, RootDepthMinimum, Precipitation_Minimum, Precipitation_Maximum, BloomPeriod } = req.body
+  let token;
+  let decoded;
+  let isAdmin;
+  let loggedIn = false;
+  if (req.cookies.loggedIn) {
+    loggedIn = true;
+    token = req.cookies.loggedIn;
+    decoded = jwt.verify(token, secret, { complete: true });
+    isAdmin = decoded.payload.isAdmin;
+  }
+
+  let plants = [];
+  plants = await Plant.find({name: "CommonName"}).limit(4);
+  console.log(plants)
+  res.render("garden", { title: "Plants", plants, loggedIn, isAdmin });
+});
+
+module.exports = router;
