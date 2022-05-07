bf1d96922bebacf46ee5f8bf957cf60cb408fc31
NicoCodes86 <paolocamacho423@gmail.com>
2022-05-07 18:20:33 -0500

    updating changes

---
+++ models/Article.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
+
+const articleSchema = Schema({
+    title: {
+        type: String, 
+        required: [true, "Title required!"],
+        trim: true
+    },
+    author: {
+        type: String,
+        required: [true, "Author required!"],
+        trim: true, 
+    },
+    description: {
+        type: String,
+        required: [true, "Description required!"],
+        trim: true,
+    },
+    link: {
+        type: String, 
+        trim: true,
+    },
+    imageUrl: String, 
+})
+
+module.exports = mongoose.model('Article', articleSchema);
