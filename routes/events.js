5c715d3044abb0f5ad90be01c861a80361c68340
NicoCodes86 <paolocamacho423@gmail.com>
2022-05-07 18:52:30 -0500

    still updating branch

--- routes/events.js
+++ routes/events.js
@@ -1,28 +1,25 @@
-const express = require('express');
+const express = require('express'); 
 const router = express.Router();
 const Event = require('../models/Event');
 const jwt = require('jsonwebtoken');
 const secret = process.env.JWT_SECRET;
 
 router.get('/', async function (req, res) {
-  let token;
-  let decoded;
-  let isAdmin;
+  // console.log('events is firing.');
   let loggedIn = false;
   if (req.cookies.loggedIn) {
     loggedIn = true;
-    token = req.cookies.loggedIn;
-    decoded = jwt.verify(token, secret, { complete: true });
-    isAdmin = decoded.payload.isAdmin;
   }
 
   const events = await Event.find({}).lean().sort('date');
 
-  res.render('events', { events, loggedIn, isAdmin });
+  res.render('events', { events, loggedIn });
 });
 
 router.post('/', async (req, res) => {
   const { event, date, description, imageUrl } = req.body;
+  // console.log(req.body);
+  // console.log(event, date, description, imageUrl);
 
   const newEvent = new Event({
     event: event,
@@ -50,8 +47,39 @@ router.post('/', async (req, res) => {
   });
 });
 
-router.get('/signUp', (req, res) => {
-  console.log('firing!');
-});
+router.get('/update/:id', async (req, res) => {
+  console.log("firing");
+  // console.log(2222222, res.body)
+  const event = await Event.findById(req.params.id)
+  console.log(1111111, event)
 
-module.exports = router;
+  res.render('updateEvent', {title: "Update event", event})
+})
+
+router.post('/update/:id', async(req, res) => {
+  const eventId = req.params.id;
+  const event = await Event.findById(eventId, req.body).exec()
+
+  await event.save((err) => {
+    if(err) {
+      console.log(err)
+    } else {
+      Event.findByIdAndUpdate(eventId, event).exec()
+      console.log("Event successfully updated, check your DB!")
+      res.redirect('/events'); 
+    }
+  })
+})
+
+router.get('/delete/:id', async(req,res) => {
+  const event = await Event.findById(req.params.id);
+
+  res.render('deleteEvent', {title:"Delete Event", event})
+})
+
+router.post('/delete/:id', async(req, res) => {
+  const eventId = req.params.id;
+  const event = await Event.findByIdAndDelete(eventId);
+
+  res.redirect('/events')
+})
