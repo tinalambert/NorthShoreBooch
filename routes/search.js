52e33cd8ba0178a6ff014e16a2e307af996caa67
NicoCodes86 <paolocamacho423@gmail.com>
2022-05-09 19:46:09 -0500

    cleaning up code

--- routes/search.js
+++ routes/search.js
@@ -4,7 +4,6 @@ const Product = require('../models/Product');
 const Event = require('../models/Event');
 
 router.get('/', async (req, res) => {
-  // console.log('the search is: ' + req.query.search)
 
   const productName = req.query.search;
   const eventName = req.query.search;
@@ -13,7 +12,6 @@ router.get('/', async (req, res) => {
   let events = [];
 
   if (!productName || !eventName) {
-    // console.log(1)
     return res.render('searchResults', { message: 'No results found!' });
   }
   if (productName || eventName) {
@@ -26,7 +24,6 @@ router.get('/', async (req, res) => {
 
     return res.render('searchResults', { products, events });
   }
-  // console.log(5)
   res.redirect('/');
 });
 
